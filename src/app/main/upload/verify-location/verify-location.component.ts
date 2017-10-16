e8939b70faa73421e89b91d61751e1121f0c8f66
Jeff <Haske107@mail.chapman.edu>
2017-10-16 16:47:13 -0700

    Component ready for testing with parent

--- src/app/main/upload/verify-location/verify-location.component.ts
+++ src/app/main/upload/verify-location/verify-location.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {MapService} from '../../../services/map.service';
+import {inlineInterpolate} from "@angular/core/src/view";
 
 @Component({
     selector: 'app-verify-location',
@@ -9,16 +10,26 @@ import {MapService} from '../../../services/map.service';
 })
 export class VerifyLocationComponent implements OnInit {
 
-    lat = 33.78595702552393;
-    lng = -117.85323192626953;
+    @Output() locationFound = new EventEmitter<any>();
     @ViewChild('googlemaps') googlemap ;
 
 
+    height =  '0px';
+    display = 'none';
+    buttonDisplay = 'none';
+    doneDisplay = 'none';
+    title = "Is this your property?";
+    drag = false;
+    lat = 33.78595702552393;
+    lng = -117.85323192626953;
+    mlat: number;
+    mlng: number;
     Street = '';
     City  = '';
     State  = '';
     Country  = '';
     Postal  = '';
+
 
     constructor(private mapService: MapService) {
 
@@ -106,11 +117,45 @@ export class VerifyLocationComponent implements OnInit {
                         this.lat = data.results[0].geometry.location.lat;
                         this.lng = data.results[0].geometry.location.lng;
                         this.googlemap.triggerResize();
+                        this.height = '50px';
+                        this.display = 'inline';
+                        this.buttonDisplay = 'inline';
                     }
                 );
         }
-
-        //wait for resposne
-        //change map position
     }
+
+    ifYes() {
+        this.locationFound.emit({lat: this.lat, lng: this.lng});
+    }
+    ifNo()  {
+        this.drag = true;
+        this.title ="Drag marker into place.";
+        this.buttonDisplay = 'none';
+        this.doneDisplay = 'inline';
+    }
+    done()  {
+        this.display = 'none';
+        this.doneDisplay = 'none';
+        this.drag = false;
+        this.locationFound.emit({lat: this.mlat, lng: this.mlng});
+    }
+    setMarkerCoords(event: any)   {
+        this.mlat = event.coords.lat;
+        this.mlng = event.coords.lng;
+    }
+    getCurrentLocation()    {
+        navigator.geolocation.getCurrentPosition(position => {
+            this.lat = position.coords.latitude;
+            this.lng = position.coords.longitude;
+            this.googlemap.triggerResize();
+            this.height = '50px';
+            this.display = 'inline';
+            this.buttonDisplay = 'inline';
+        });
+    }
+
+
+
+
 }
