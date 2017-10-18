03c720716b5300cdd7f684abd371278066cd8e0c
LilianaHernandez1 <herna181@mail.chapman.edu>
2017-10-18 14:17:59 -0700

    seperating fill out details to fill out details and rules

--- src/app/main/upload/fill-out-details/fill-out-details.component.ts
+++ src/app/main/upload/fill-out-details/fill-out-details.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Host, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UploadComponent} from "../upload.component";
 
 @Component({
   selector: 'app-fill-out-details',
@@ -9,9 +10,21 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class FillOutDetailsComponent implements OnInit {
 
   // Jason and Lili
-
-  @Output() idEmitter = new EventEmitter<string>();
-  LocationData: FormGroup;
+    private addr = '';
+    private city = '';
+    private state = '';
+    private zip = '';
+    private country = '';
+    private propertyTitle = '';
+    private locationType = '';
+    private locationDescription = '';
+    private numBathrooms = 0;
+    private numParking = 0;
+    private electricity = false;
+    private desposit = 0.0;
+    private ratePerDay = 0.0;
+    @Output() idEmitter = new EventEmitter<string>();
+    LocationData: FormGroup;
     Types = [
         {value: 'Home'},
         {value: 'Business'},
@@ -21,7 +34,14 @@ export class FillOutDetailsComponent implements OnInit {
         {value: 'Public Space'}
     ];
 
-  constructor() { }
+  constructor(@Host() parent: UploadComponent) {
+
+      this.addr = parent.propertyAddress;
+      this.city = parent.propertyCity;
+      this.state = parent.propertyState;
+      this.zip = parent.propertyZip;
+      this.country = parent.propertyCountry;
+  }
 
   ngOnInit() {
 
@@ -40,5 +60,31 @@ export class FillOutDetailsComponent implements OnInit {
           electricity: new FormControl(null , Validators.required),
           parking: new FormControl(null , Validators.required),
       });
+
+      this.LocationData.get('streetAddress').setValue(this.addr);
+      this.LocationData.get('city').setValue(this.city);
+      this.LocationData.get('state').setValue(this.state);
+      this.LocationData.get('zip').setValue(this.zip);
+      this.LocationData.get('country').setValue(this.country);
   }
+
+    incrementBathroom() {
+      ++this.numBathrooms;
+    }
+
+    decrementBathroom() {
+        if (this.numBathrooms >= 1) {
+            --this.numBathrooms;
+        }
+    }
+
+    incrementParking() {
+        ++this.numParking;
+    }
+
+    decrementParking() {
+        if (this.numParking >= 1) {
+            --this.numParking;
+        }
+    }
 }
--- src/app/main/upload/upload.component.ts
+++ src/app/main/upload/upload.component.ts
@@ -20,6 +20,27 @@ export class UploadComponent implements OnInit {
     locationID = '';
     state = 0;
 
+    propertyAddress = '1 University Dr';
+    propertyZip = '92867';
+    propertyCity = 'Orange';
+    propertyState = 'CA';
+    propertyCountry = 'USA';
+
+
+    // from upload component
+    propertyTitle = '';
+    locationType = '';
+    locationDescription = '';
+    numBathrooms = 0;
+    numParking = 0;
+    electricity = false;
+    desposit = 0.0;
+    ratePerDay = 0.0;
+
+    // from rules component
+    rulesArr = [];
+
+
     constructor(private authService: AuthService, private router: Router, private snackBar: MdSnackBar)   {
 
     }
@@ -27,6 +48,7 @@ export class UploadComponent implements OnInit {
     ngOnInit()  {
 
     }
+
     setID(ID: string){
       this.locationID = ID;
       this.state = 1;
