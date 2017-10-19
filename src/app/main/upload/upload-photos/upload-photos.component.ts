fe2c5e5d47e4833dc16e9d695279f3568089160e
Jeff <Haske107@mail.chapman.edu>
2017-10-18 18:19:46 -0700

    How image upload can work.

--- src/app/main/upload/upload-photos/upload-photos.component.ts
+++ src/app/main/upload/upload-photos/upload-photos.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
-import {FileService} from "../../../services/file.service";
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {FileService} from '../../../services/file.service';
+import {Location} from '../../../ts models/location.model';
 
 @Component({
   selector: 'app-upload-photos',
@@ -8,22 +9,42 @@ import {FileService} from "../../../services/file.service";
 })
 export class UploadPhotosComponent implements OnInit {
 
+    // GAIN LIVE ACCESS TO THE HTML FILE INPUT ELEMENT
+    @ViewChild('FileInput') _FileInput: ElementRef;
+
+    // AN INSTANCE OF THE CURRENT LOCATION BEING UPLOADED IS INJECTED INTO THIS COMPONENT
+        // HAS TO HAVE BEEN INITILIAZED BY THE DATABASE IN ORDER TO RECEIVE UNIQUE ID
+    @Input()location: Location;
 
   constructor(private fileService: FileService) { }
 
   ngOnInit() {
   }
 
-  onChange(event) {
-        let fileForm = new FormData();
-        let file = event.srcElement.files;
-        fileForm.append('photo', file , file.name);
-        this.fileService.uploadPhoto('TESTING', fileForm)
+    uploadFile() {
+      // CREATE NEW EMPTY FORM-DATA INSTANCE
+        const formData = new FormData();
+      // STORE FILES CURRENTLY STORED IN OUR HTML INPUT ELEMENT
+            // IT WOULD BE GOOD TO CHECK IF ANYTHING IS INSIDE FIRST
+        const FileList = this._FileInput.nativeElement.files;
+      // APPEND EACH FILE TO THE FORM-DATA
+        // ASSIGN KEYWORD PHOTO FOR THE SERVER
+            // MAKE A FOR LOOP TO MAKE IT MORE DYNAMIC
+        formData.append('photo', FileList[0]);
+    // LETS DOUBLE CHECK THAT THE INJECTED LOCATION HAS AN ID
+        if (!this.location._id)   {
+            // FOR NOW, SINCE WE WONT HAVE ONE INJECTED FOR DEV PURPOSES, WE WILL REPLACE IT
+            this.location._id = 'testing-DELETE ME ASAP';
+        }
+      // CALL THE MAGICAL FILE SERVICE TO SEND IT INTO WONDERLAND
+        this.fileService.uploadPhoto( this.location._id, formData)
             .subscribe(
                 data =>  {
+                    // 201 SOMETHING GOOD HAPPENED
                 console.log(data);
             },
                 error =>    {
+                    // 400-500 YOU PROBABLY AREN'T SIGNED IN, ONLY ACTIVE USERS CAN POST
                     console.error(error);
                 }
             );
