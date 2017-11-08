f96c1957bbff0adfe617f9bb1134e165266497be
Jeff <Haske107@mail.chapman.edu>
2017-11-08 15:24:33 -0800

    Fixing update issues

--- src/app/main/collections/community-collections/community-collections.component.ts
+++ src/app/main/collections/community-collections/community-collections.component.ts
@@ -1,7 +1,6 @@
 import {AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
 import {CollectionService} from "../../../services/collection.service";
 import {Collection} from "../../../ts models/collection.model";
-import  {_} from "underscore"
 import {MatDialog} from "@angular/material";
 import {NewCollectionComponent} from "../new-collection/new-collection.component";
 
@@ -30,15 +29,7 @@ export class CommunityCollectionsComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit()  {
-    this.collectionService.getPublicCollections()
-      .subscribe(
-        (collections: Collection[]) => {
-          let userID = localStorage.getItem('userID');
-          this.collections = collections;
-          this.subscribedcollections = _.filter(this.collections, collection => {
-            return _.contains(collection.followers, userID)
-          });
-        });
+
   }
 
   onChange()  {
